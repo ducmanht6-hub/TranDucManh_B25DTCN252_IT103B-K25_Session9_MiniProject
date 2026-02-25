cdf69aea724557f614ebe7a8e4d15ba88d791824
ducmanht6-hub <ducmanht6@gmail.com>
2026-02-25 13:48:15 +0700

    Update ss9.js

--- session 09/ss9.js
+++ session 09/ss9.js
@@ -1,11 +1,10 @@
 let orders = ["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", "Đơn hàng E"];
 let revenues = [1500, 2800, 1200, -500, 3200];
-let orderReports = [];
-
-for (let i = 0; i < orders.length; i++) {
-    let report = `${orders[i]} mang về ${revenues[i]} USD`;
-    orderReports.push(report);
-}
+let orderReports = orders.map((order, index) => 
+    `${order} mang về ${revenues[index]} USD`
+);
 
 console.log(orderReports);
+
+
 
